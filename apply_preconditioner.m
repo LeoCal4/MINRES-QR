818e2928ae026706978b4a270343697979c8696e
LeoCal4 <linkindayd@gmail.com>
2022-10-14 16:31:51 +0200

    Removed truncation in precon minres, fixed precon sparsity

--- apply_preconditioner.m
+++ apply_preconditioner.m
@@ -4,17 +4,16 @@ function [D_s, C, S, P] = apply_preconditioner(A, size_D, sparse, threshold)
     end
     if exist('threshold', 'var') == 0
        threshold = 0;
-    end 
+    end
     D = A(1:size_D, 1:size_D);
     E = A(size_D+1:end, 1:size_D);
-    %fprintf("density(E): %2.5f\n", (nnz(E)*100)/(size(E, 1)*size(E, 2)));
-    S = -E * (D \ E'); % a possible sparse approximation to do is to have nnz only where E has them
-    %fprintf("density(S): %2.5f\n", (nnz(S)*100)/(size(S, 1)*size(S, 2)));
+    fprintf("density(E): %2.5f\n", (nnz(E)*100)/(size(E, 1)*size(E, 2)));
+    S = -E * (D \ E');
+    fprintf("density(S): %2.5f\n", (nnz(S)*100)/numel(S));
     if sparse == true
-        E_mask = E(:, 1:size(E, 1)) ~= 0;
-        S = S .* E_mask;
-        %fprintf("post mask density(S): %2.5f\n", (nnz(S)/(size(S, 1)*size(S, 2)))*100);
-        %S(S < threshold & S > -threshold) = 0;
+        non_diag_mask = ~logical(eye(size(S)));
+        S(non_diag_mask & S < threshold & S > -threshold) = 0;
+        fprintf("post mask density(S): %2.5f\n", (nnz(S)*100)/numel(S));
     end
     D_s = sqrt(D);
     C = chol(-S);
--- minres_qr.m
+++ minres_qr.m
@@ -1,64 +1,56 @@
 function [x, residuals, iter, residuals_precon] = minres_qr(A, b, reorthogonalize, precon, size_D)
+    % initialize optional variables
     if exist('reorthogonalize', 'var') == 0
        reorthogonalize = false;
     end
     if exist('precon', 'var') == 0
        precon = false;
     end
+    % check whether A is symmetric
     if ~is_symm(A)
-        disp('A DEVE ESSERE SIMMETRICA OOOOOOOO')
+        error('A must be symmetric')
         return
     end
-    
-    % saving the original A and b
+    % saving the original b, before eventual preconditioning
     b_start = b;
-    A_start = A;
-    
     % preconditioning 
     D_s = 0;
     C = 0;
     if precon == true
-        A = A(1:end-1, 1:end-1);
-        b = b(1:end-1, :);
         [D_s, C] = apply_preconditioner(A, size_D);
         b = multiply_preconditioner(b, D_s, C, true);
         residuals_precon = nan(1, size(A, 1));
     end
     
+    % initialized general vars
     size_A = size(A, 1);
     threshold = 1e-9;
     residuals = nan(1, size_A);
     
     % initialize Lanczos' matrices
-    V = zeros(size_A, size_A+1); % not sparse
-    T = zeros(size_A+1, size_A); % sparse - prob not worth since most of the operations are just access and no mults
+    V = zeros(size_A, size_A+1);
+    T = zeros(size_A+1, size_A);
     prev_w = b;
     beta_1 = norm(b);
     
     % initialize QR matrices
-    Q = eye(1); % not sparse
-    R = eye(1); % sparse - needs to be assigned to create T_k in QR, so not worth to make it sparse
+    Q = eye(1);
+    R = eye(1);
     c = zeros(1, 1);
     
     % iterate up to size of A at max
     for k = 1:size_A
-%         fprintf("Iteration %i\n", k);
-        
         % LANCZOS
         [V, T, prev_w] = iterative_lanczos(A, V, T, prev_w, k, reorthogonalize, precon, D_s, C);
-        
         % QR
         [Q, R] = iterative_QR(T(1:k+1, 1:k), Q, R, k);
-        
         % BACKSUBSTITUTION
         %  use the reduced version of the QR, using just Q_1 and R_1
         %      from Q = [Q_1 Q_2] and R = [R_1; 0]
         c(k, 1) = beta_1 * Q(1, k);
         y_k = R(1:end-1, :) \ c; % O(n)
-        
         % SOLUTION
         x_k = V(:, 1:k) * y_k;
-        
         if precon == true
             % solution of the original truncated system
             x_k = multiply_preconditioner(x_k, D_s, C); % x = B^-1 * \hat(x)
@@ -68,11 +60,11 @@ function [x, residuals, iter, residuals_precon] = minres_qr(A, b, reorthogonaliz
             residuals_precon(k) = norm(b - y_p);
             x_k(end+1) = 0; % solution of the original system
         end
-        
         % A*x of the original system
-        y = A_start * x_k;
+        y = A * x_k;
         residual = norm(b_start - y);
         residuals(k) = residual;
+        % check whether the residual is small enough
         if precon == false && residual <= threshold
             break
         elseif precon == true && residuals_precon(k) <= threshold
